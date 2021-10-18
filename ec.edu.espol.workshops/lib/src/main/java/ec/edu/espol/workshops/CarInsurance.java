d1e4ae41b8accd8b7bfc5511d4818238945288da
Daniel Viscarra <dviscarr@espol.edu.ec>
2021-10-17 19:49:53 -0500

    Creation of Class CarInsurance

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -0,0 +1,17 @@
+package ec.edu.espol.workshops;
+
+public class CarInsurance {
+	
+	private int age_customer;
+	private String sex_customer;
+	private String marital_status;
+	
+	private final int BASE_PREMIUM = 500;
+	private final int MALE_SINGLE = 1500;
+	private final int FEMALE_MARRIED = 200;
+	private final int OLDER_YOUNGER = 100;
+
+	
+	
+	
+}
