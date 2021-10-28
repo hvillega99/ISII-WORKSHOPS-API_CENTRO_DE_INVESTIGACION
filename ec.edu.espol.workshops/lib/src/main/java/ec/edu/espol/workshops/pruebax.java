624dacd5ceaeb08418c0a552c8d2f5a9fa266d38
Daniel Viscarra <dviscarr@espol.edu.ec>
2021-10-28 17:55:08 -0500

    First attempt of test

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/pruebax.java
@@ -0,0 +1,10 @@
+package ec.edu.espol.workshops;
+
+public class pruebax {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("tutancamos");
+    }
+
+}
