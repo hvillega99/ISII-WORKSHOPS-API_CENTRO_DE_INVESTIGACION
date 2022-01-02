ca8731ff1ea4c2be69ae45c53f05a9bd8211aac4
hvillega99 <hvillega@espol.edu.ec>
2022-01-02 18:00:49 -0500

    Branch Coverage Test corrected

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/BranchCoverageTest.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/BranchCoverageTest.java
@@ -9,13 +9,13 @@ public class BranchCoverageTest {
     
     public BranchCoverageTest() {
         carInsurance = new CarInsurance();
-        carInsurance.setAge_customer(44);
-        carInsurance.setSex_customer("M");
-        carInsurance.setMarital_status("not married");
     }
     
     @Test
     public void executeTest() {
+        carInsurance.setAge_customer(44);
+        carInsurance.setSex_customer("M");
+        carInsurance.setMarital_status("not married");
         assertEquals(500, this.carInsurance.totalCalc());
     }
 }
