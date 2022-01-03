06234fcd76094191557ff935b46c49d8fa1276b8
Carlos <carlosqa73@hotmail.com>
2022-01-03 14:54:08 -0500

    Corrections

--- ec.edu.espol.workshops/lib/src/test/java/pruebas/BranchCoverageTest.java
+++ ec.edu.espol.workshops/lib/src/test/java/pruebas/BranchCoverageTest.java
@@ -15,19 +15,24 @@ public class BranchCoverageTest {
     public void executeTest() {
         
         carInsurance.setAge_customer(24);
-        carInsurance.setSex_customer("M");
-        carInsurance.setMarital_status("not married");
-        assertEquals(500, this.carInsurance.totalCalc());
-        
-        carInsurance.setAge_customer(50);
         carInsurance.setSex_customer("F");
         carInsurance.setMarital_status("married");
+        assertEquals(300, this.carInsurance.totalCalc());
+        
+        carInsurance.setAge_customer(30);
+        carInsurance.setSex_customer("M");
+        carInsurance.setMarital_status("not married");
         assertEquals(300, this.carInsurance.totalCalc());
         
         carInsurance.setAge_customer(18);
         carInsurance.setSex_customer("M");
         carInsurance.setMarital_status("not married");
         assertEquals(-1, this.carInsurance.totalCalc());
+        
+        carInsurance.setAge_customer(23);
+        carInsurance.setSex_customer("M");
+        carInsurance.setMarital_status("married");
+        assertEquals(500, this.carInsurance.totalCalc());
         
     }
     
