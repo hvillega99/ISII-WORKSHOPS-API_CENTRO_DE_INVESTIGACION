827296ce3a4d77fadb5a6a0f28f1ce0c192b37ea
Carlos <carlosqa73@hotmail.com>
2022-01-03 12:08:51 -0500

    Branch Coverage test code

---
+++ ec.edu.espol.workshops/lib/src/test/java/pruebas/BranchCoverageTest.java
@@ -0,0 +1,34 @@
+package pruebas;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ec.edu.espol.workshops.*;
+
+public class BranchCoverageTest {
+
+    final CarInsurance carInsurance;
+    
+    public BranchCoverageTest() {
+        carInsurance = new CarInsurance();
+    }
+    
+    @Test
+    public void executeTest() {
+        
+        carInsurance.setAge_customer(24);
+        carInsurance.setSex_customer("M");
+        carInsurance.setMarital_status("not married");
+        assertEquals(500, this.carInsurance.totalCalc());
+        
+        carInsurance.setAge_customer(50);
+        carInsurance.setSex_customer("F");
+        carInsurance.setMarital_status("married");
+        assertEquals(200, this.carInsurance.totalCalc());
+        
+        carInsurance.setAge_customer(18);
+        carInsurance.setSex_customer("M");
+        carInsurance.setMarital_status("not married");
+        assertEquals(-1, this.carInsurance.totalCalc());
+        
+    }
+    
+}
